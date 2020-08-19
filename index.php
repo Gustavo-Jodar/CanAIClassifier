3745b726ecfac3ad9140c3552fa7deb6bd0ae0dd
gustavo.jodar <gustavo.jodar@catijr.com.br>
2020-08-18 23:47:59 -0300

    Adiciona um estilinho

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Identificador de Latinhas</title>
+</head>
+<body>
+<div class="jumbotron">
+  <h1 class="display-4">Artificial intelligence to identify can brands through photos</h1>
+  <p class="lead">This AI uses the KNN (k-Nearst Neighbors) algorithm to classificate cans photos, it's very simple and sometimes even dump.
+    So, it helps if the photo has a white background, if you try to upload a photo of another thing, that is not a can, the AI will try to classificate it as a can.
+  </p>
+  <hr class="my-4">
+  <p>You can upload your can photo through the button bellow.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Upload photo</a>
+</div>
+</body>
+
+</html>
